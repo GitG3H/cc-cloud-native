643de39f50ec0d8b8524cc8750efc2ed53c96e84
Bharath Pulindram <bharath.pulindram@gmail.com>
2023-04-27 21:17:11 -0500

    firebase changes

---
+++ src/Components/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyClueRLNB3-wIhawmXqDru5KVMF28_3i_A",
+  authDomain: "cloud-native-chat.firebaseapp.com",
+  projectId: "cloud-native-chat",
+  storageBucket: "cloud-native-chat.appspot.com",
+  messagingSenderId: "1099477711985",
+  appId: "1:1099477711985:web:4b3078d38f0e7348582fd0",
+  measurementId: "G-G4Y8FV8C2N",
+};
