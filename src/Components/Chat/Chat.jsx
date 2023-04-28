6ab654db65a81e0fe5e7e9aa6f4636054cc7ccb6
Bharath Pulindram <bharath.pulindram@gmail.com>
2023-04-28 16:05:04 -0500

    chat functionality changes

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -5,9 +5,13 @@ import { IconButton } from "@mui/material";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import AttachFileIcon from "@mui/icons-material/AttachFile";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+//import MicIcon from "@mui/icons-material/Mic";
+import SendIcon from "@mui/icons-material/Send";
 
 function Chat() {
   const [seed, setSeed] = useState("");
+  const [input, setInput] = useState("");
   const [avatarList] = useState([
     "Peanut",
     "Daisy",
@@ -23,6 +27,11 @@ function Chat() {
     "Willow",
     "Boots",
   ]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    setInput("");
+  };
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * avatarList.length));
@@ -51,8 +60,27 @@ function Chat() {
           </IconButton>
         </div>
       </div>
-      <div className="chat__body"></div>
-      <div className="chat__footer"></div>
+      <div className="chat__body">
+        <p className={`chat__message ${true && "chat__receiver"}`}>
+          <span className="chat__name">Bharath</span>
+          Hey Guys
+          <span className="chat__timestamp">1.33 pm</span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input
+            value={input}
+            type="text"
+            placeholder="Send a message.."
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button onClick={sendMessage} className="submitBtn" type="submit">
+            <SendIcon />
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
